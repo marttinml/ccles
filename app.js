ef23bf5048406cb68039e4bbfe519f2396357595
marttinml <martin.ml@icloud.com>
2014-12-12 23:40:39 -0600

    update port

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ app.use(express.static(__dirname + '/www'));
 
 routes = require('./routes/ccles')(app);
 
+var port = process.env.PORT || 5000;
 
-	server.listen(2000, function() {
+	server.listen(port, function() {
   		console.log(" - - - - - - - - - - - - - - - - - - - - - - - ");
       	console.log(" |     API REST - http://localhost:2000      | ");
       	console.log(" - - - - - - - - - - - - - - - - - - - - - - - ");
