5fda6e6c2fe5563749804f470571602d383bf0fe
marttinml <martin.ml@icloud.com>
2014-12-12 23:19:18 -0600

    update url service
    se actualizo url heroku

--- routes/ccles.js
+++ routes/ccles.js
@@ -18,10 +18,9 @@ init = function(req, res){
 };
 
 	newUser =  function(req, res){
-		
 		console.log(req.body);
 		console.log('cresting user...');
-	  	client.post("http://locahost:5000/user",req.body,function(data,response){
+	  	client.post("https://polar-fortress-3883.herokuapp.com/user",req.body,function(data,response){
 			console.log('successful');
 			res.send(":)");
 		});
@@ -32,7 +31,7 @@ init = function(req, res){
 		
 		console.log(req.body);
 		console.log('getting sesion...');
-	  	client.get("http://localhost:5000/getsesion/2491196788",function(data,response){
+	  	client.get("https://polar-fortress-3883.herokuapp.com/getsesion/2491196788",function(data,response){
 			console.log(data);
 			res.send(JSON.parse(data));
 		});
